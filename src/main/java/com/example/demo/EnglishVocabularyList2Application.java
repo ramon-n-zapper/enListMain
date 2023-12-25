e33216fbfe575be6d85a90ad7f0dc85cef8e481f
rakuyou <javaleanerse@gmail.com>
2023-12-25 14:09:37 +0900

    初めてのコミットです。アプリの根幹の機能はほぼ実装済みですが、セキュリティ面はチェックしていません。

---
+++ src/main/java/com/example/demo/EnglishVocabularyList2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishVocabularyList2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishVocabularyList2Application.class, args);
+	}
+
+}
