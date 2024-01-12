79da93cf03a9c88d2b9c4caafe41126a70c113cc
rakuyou <javaleanerse@gmail.com>
2024-01-12 15:13:02 +0900

    Login.htmlに入力された値をコンソールに表示できるようにしました。

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,5 +1,23 @@
 package com.example.demo.controller;
 
-public class LoginController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.form.LoginForm;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping("/login")
+	public String view(Model model, LoginForm form) {
+		
+		return "login";
+	}
+	
+	@PostMapping("/login")
+	public void login(LoginForm form) {
+		System.out.println(form.toString());
+	}
 }
