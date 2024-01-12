fbad71b46a17893bc8f1f5cca08db2800f12a72e
rakuyou <javaleanerse@gmail.com>
2024-01-12 17:00:33 +0900

    仮のログイン機能を作成しました

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -10,6 +10,10 @@ import com.example.demo.form.LoginForm;
 @Controller
 public class LoginController {
 	
+	private static final String LOGIN_ID = "user";
+	
+	private static final String PASSWORD = "pwd";
+	
 	@GetMapping("/login")
 	public String view(Model model, LoginForm form) {
 		
@@ -17,7 +21,14 @@ public class LoginController {
 	}
 	
 	@PostMapping("/login")
-	public void login(LoginForm form) {
-		System.out.println(form.toString());
+	public String login(Model model, LoginForm form) {
+		var isCorrectUserAuth = form.getLoginId().equals(LOGIN_ID) 
+				&& form.getPassword().equals(PASSWORD);
+		if(isCorrectUserAuth) {
+			return "redirect:/";
+		} else {
+			model.addAttribute("errorMsg", "ログインIDとパスワードが間違っています");
+			return "login";
+		}
 	}
 }
