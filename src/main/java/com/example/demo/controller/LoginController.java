e713fdebd4a3972453230876ccddcb42935dd060
ramon-n-zapper <javaleanerse@gmail.com>
2024-01-24 17:19:05 +0900

    パスワードをハッシュ化してDB登録出来るようにしました

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,13 +1,11 @@
 package com.example.demo.controller;
 
-import java.util.Optional;
-
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.example.demo.entity.UserInfo;
 import com.example.demo.form.LoginForm;
 import com.example.demo.service.LoginService;
 
@@ -25,6 +23,9 @@ public class LoginController {
 	
 	/**ログイン画面 Service*/
 	private final LoginService service;
+	
+	/** PasswordEncoder */
+	private final PasswordEncoder passwordEncoder;
 	
 	/**
 	 * 初期表示
@@ -48,9 +49,9 @@ public class LoginController {
 	 */
 	@PostMapping("/login")
 	public String login(Model model, LoginForm form) {
-		Optional<UserInfo> userInfo = service.searchUserById(form.getLoginId());
+		var userInfo = service.searchUserById(form.getLoginId());
 		boolean isCorrectUserAuth = userInfo.isPresent()
-				&& form.getPassword().equals(userInfo.get().getPassword());
+				&& passwordEncoder.matches(form.getPassword(), userInfo.get().getPassword());	
 		if(isCorrectUserAuth) {
 			return "redirect:/";
 		} else {
